8d9f67bd3e1fa20a348c25a5e46aa1d4e51c3b51
Juan Escalada <juanescalada175@gmail.com>
2022-12-22 17:06:47 -0800

    Fix reports service test

--- src/reports/reports.service.spec.ts
+++ src/reports/reports.service.spec.ts
@@ -1,15 +1,49 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ReportsService } from './reports.service';
+import { Repository } from 'typeorm';
+import { Report } from './report.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+
+const reportArray = [
+  {
+    id: 1,
+    price: 5000,
+    lat: 10,
+    lng: 10,
+    make: 'Toyota',
+    model: 'Corolla',
+    year: 2005,
+    approved: true,
+    mileage: 2000,
+    user: {
+      id: 1,
+      email: 'test@test.com',
+      password: 'testpass',
+      admin: true,
+      reports: [],
+    },
+  },
+];
 
 describe('ReportsService', () => {
   let service: ReportsService;
+  let repo: Repository<Report>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ReportsService],
+      providers: [
+        ReportsService,
+        {
+          provide: getRepositoryToken(Report),
+          useValue: {
+            find: jest.fn().mockResolvedValue(reportArray),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<ReportsService>(ReportsService);
+    repo = module.get<Repository<Report>>(getRepositoryToken(Report));
   });
 
   it('should be defined', () => {

c766bde456a4a3e2e07f9cfdf383930b6976f838
Juan Escalada <juanescalada175@gmail.com>
2022-12-22 16:23:44 -0800

    Fix user service test

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,15 +1,37 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+
+const userArray = [
+  {
+    email: 'testuser@test.com',
+    password: 'testhashedpassword',
+    id: 1,
+    admin: false,
+  },
+];
 
 describe('UsersService', () => {
   let service: UsersService;
+  let repo: Repository<User>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: {
+            find: jest.fn().mockResolvedValue(userArray),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+    repo = module.get<Repository<User>>(getRepositoryToken(User));
   });
 
   it('should be defined', () => {

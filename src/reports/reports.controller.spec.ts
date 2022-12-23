a60a0bb0b27596c719db81025da681f7c383461b
Juan Escalada <juanescalada175@gmail.com>
2022-12-22 17:02:40 -0800

    Fix reports controller test

--- src/reports/reports.controller.spec.ts
+++ src/reports/reports.controller.spec.ts
@@ -1,12 +1,42 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ReportsController } from './reports.controller';
+import { ReportsService } from './reports.service';
 
 describe('ReportsController', () => {
   let controller: ReportsController;
+  let mockReportsService: Partial<ReportsService>;
 
   beforeEach(async () => {
+    mockReportsService = {
+      changeApproval: (id: string, approved: boolean) => {
+        return Promise.resolve({
+          id: 1,
+          price: 5000,
+          lat: 10,
+          lng: 10,
+          make: 'Toyota',
+          model: 'Corolla',
+          year: 2005,
+          approved: true,
+          mileage: 2000,
+          user: {
+            id: 1,
+            email: 'test@test.com',
+            password: 'testpass',
+            admin: true,
+            reports: [],
+          },
+        });
+      },
+    };
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ReportsController],
+      providers: [
+        {
+          provide: ReportsService,
+          useValue: mockReportsService,
+        },
+      ],
     }).compile();
 
     controller = module.get<ReportsController>(ReportsController);
